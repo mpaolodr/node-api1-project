e9580c55a8f3764fadb7b5b7463a9034ea122233
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-10 15:48:54 -0500

    get usesr by id working

--- index.js
+++ index.js
@@ -17,7 +17,30 @@ server.get("/api/users", (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(404).json({ message: "No users found" });
+      res.status(500).json({
+        errorMessage: "The users information could not be retrieved."
+      });
+    });
+});
+
+// view user by id
+server.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ errorMessage: "The user information could not be retrieved." });
     });
 });
 
