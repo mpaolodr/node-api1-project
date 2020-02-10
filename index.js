0c7706f913d3c62b1807acb2faa9ae09c60a1022
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-10 15:01:47 -0500

    created server and had it running

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+// import express and db
+const express = require("express");
+const Users = require("./data/db.js");
+
+// create server
+const server = express();
+
+// middleware
+server.use(express.json());
+
+// port
+const port = 8000;
+server.listen(port, () => {
+  console.log(`\n ** Server listening on port: ${port} \n`);
+});
